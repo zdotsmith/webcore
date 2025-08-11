50d7caeb5ae0a90e4872b2a5ad4cdfd88f16c2c6
zdotsmith <zsmithwk@outlook.com>
2025-08-11 22:09:04 +0000

    commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+        ssr: {
+            noExternal: ['webcoreui']
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler'
+                }
+            }
+        }
+    }
+})
